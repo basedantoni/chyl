3c7b36e6bf6191a2e7dccbf2f6c64a3cad500644
anthony.mercado <anthony.mercado@atmosphere.tv>
2024-03-14 18:57:49 -0500

    remove some stuff

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -2,17 +2,6 @@ import VideoTiles from "@/components/video/VideoTiles";
 import { getAllVideos } from "@/lib/api";
 import styles from "./styles.module.css";
 
-async function getVideos() {
-  const spaceId = "4nwbvve7a2v0";
-  const environmentId = "master";
-  const contentType = "video";
-
-  const res = await fetch(
-    `https://cdn.contentful.com/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=${contentType}&include=1`,
-  );
-  return res.json();
-}
-
 export default async function Video() {
   const allVideos = await getAllVideos();
 
