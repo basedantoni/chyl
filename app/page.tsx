3b198b9a93a16fcdf4ffe220c6cbd33177da748e
anthony.mercado <anthony.mercado@atmosphere.tv>
2024-04-30 09:25:27 -0500

    landing page marquee poc

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import VideoBackground from '@/components/VideoBackground';
-import { Video } from '@/types';
+import VideoBackground from "@/components/VideoBackground";
+import { Video } from "@/types";
 
 export default function Home() {
   const videos: Array<Video> = [
@@ -7,12 +7,12 @@ export default function Home() {
     { title: "02", url: "/video/chyl-car.mp4" },
     { title: "03", url: "/video/chyl-pokemon.mp4" },
     { title: "01", url: "/video/landing.mp4" },
-  ]
+  ];
   return (
-    <main>
-      <h1 className="font-moki absolute top-20 w-full text-center text-7xl">
+    <main className="relative h-screen w-screen overflow-hidden">
+      {/* <h1 className="font-moki absolute top-20 w-full text-center text-7xl">
         CHYL
-      </h1>
+      </h1> */}
       <VideoBackground videos={videos} />
     </main>
   );
--- components/VideoBackground.tsx
+++ components/VideoBackground.tsx
@@ -1,7 +1,9 @@
-"use client"
+"use client";
 
-import { Video } from '@/types';
-import { useState } from "react"
+import { Video } from "@/types";
+import { useState } from "react";
+import "@/app/globals.css";
+import Link from "next/link";
 
 type Props = {
   videos: Array<Video>;
@@ -13,22 +15,91 @@ export default function VideoBackground({ videos }: Props) {
   return (
     <>
       <div className="fixed left-0 top-0 -z-10 h-screen w-screen object-cover object-bottom">
-        {videoUrl && <video
-          autoPlay
-          muted
-          loop
-          className="w-full h-full object-cover"
-          src={videoUrl}
-        />}
+        {videoUrl && (
+          <video
+            autoPlay
+            muted
+            loop
+            className="w-full h-full object-cover"
+            src={videoUrl}
+          />
+        )}
       </div>
-      <div>
-        <ul className='flex gap-3 text-center text-lg'>
-          <li onMouseEnter={() => setVideoUrl(videos[0].url)} onMouseLeave={() => setVideoUrl(null)}>Music</li>
-          <li onMouseEnter={() => setVideoUrl(videos[1].url)} onMouseLeave={() => setVideoUrl(null)}>Art</li>
-          <li onMouseEnter={() => setVideoUrl(videos[2].url)} onMouseLeave={() => setVideoUrl(null)}>Video</li>
-          <li onMouseEnter={() => setVideoUrl(videos[3].url)} onMouseLeave={() => setVideoUrl(null)}>Merch</li>
+      <div className="absolute bottom-0 marquee">
+        <ul className="marquee__content text-center font-bold text-9xl uppercase hover:cursor-pointer">
+          <Link
+            className="hover:text-orange-500"
+            onMouseEnter={() => setVideoUrl(videos[0].url)}
+            onMouseLeave={() => setVideoUrl(null)}
+            href="/music"
+          >
+            Music
+          </Link>
+          <Link
+            className="hover:text-orange-500"
+            onMouseEnter={() => setVideoUrl(videos[1].url)}
+            onMouseLeave={() => setVideoUrl(null)}
+            href="/tour"
+          >
+            Tour
+          </Link>
+          <Link
+            className="hover:text-orange-500"
+            onMouseEnter={() => setVideoUrl(videos[2].url)}
+            onMouseLeave={() => setVideoUrl(null)}
+            href="/video"
+          >
+            Video
+          </Link>
+          <Link
+            className="hover:text-orange-500"
+            onMouseEnter={() => setVideoUrl(videos[3].url)}
+            onMouseLeave={() => setVideoUrl(null)}
+            href="/music"
+          >
+            Merch
+          </Link>
+        </ul>
+
+        {/* MIRROR CONTENT */}
+        <ul
+          className="marquee__content text-center font-bold text-9xl uppercase hover:cursor-pointer"
+          aria-hidden="true"
+        >
+          <Link
+            className="hover:text-orange-500"
+            onMouseEnter={() => setVideoUrl(videos[0].url)}
+            onMouseLeave={() => setVideoUrl(null)}
+            href="/music"
+          >
+            Music
+          </Link>
+          <Link
+            className="hover:text-orange-500"
+            onMouseEnter={() => setVideoUrl(videos[1].url)}
+            onMouseLeave={() => setVideoUrl(null)}
+            href="/tour"
+          >
+            Tour
+          </Link>
+          <Link
+            className="hover:text-orange-500"
+            onMouseEnter={() => setVideoUrl(videos[2].url)}
+            onMouseLeave={() => setVideoUrl(null)}
+            href="/video"
+          >
+            Video
+          </Link>
+          <Link
+            className="hover:text-orange-500"
+            onMouseEnter={() => setVideoUrl(videos[3].url)}
+            onMouseLeave={() => setVideoUrl(null)}
+            href="/music"
+          >
+            Merch
+          </Link>
         </ul>
       </div>
     </>
-  )
+  );
 }
