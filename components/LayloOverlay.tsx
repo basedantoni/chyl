bd8060674cc18c80c64b5002afc8fa2e1272170c
basedantoni <anthony.mercado300@gmail.com>
2024-05-15 12:38:09 -0500

    Fix laylo overlay

--- components/LayloOverlay.tsx
+++ components/LayloOverlay.tsx
@@ -9,22 +9,24 @@ export default function LayloOverlay() {
 
   if (isOverlayVisible) {
     return (
-      <div className="absolute left-0 top-0 flex h-screen w-screen items-center justify-center bg-black bg-opacity-25 backdrop-blur-sm">
+      <>
         <XMarkIcon
           onClick={toggleOverlay}
           className="absolute right-0 top-0 h-16 cursor-pointer bg-white stroke-black hover:bg-black hover:stroke-white"
         />
-        <iframe
-          className="w-3/4 md:w-1/4"
-          src="https://laylo.com/chyl/profile/embed"
-          width="100%"
-          height="580"
-          allowTransparency={true}
-        ></iframe>
-        <div className="absolute text-white">
-          <Socials />
+        <div className="absolute left-0 top-24 flex h-screen w-screen items-center justify-center bg-black bg-opacity-25 backdrop-blur-sm z-10">
+          <iframe
+            className="w-3/4 md:w-1/4"
+            src="https://laylo.com/chyl/profile/embed"
+            width="100%"
+            height="580"
+            allowTransparency={true}
+          ></iframe>
+          <div className="absolute text-white">
+            <Socials />
+          </div>
         </div>
-      </div>
+      </>
     );
   } else {
     return <></>;
