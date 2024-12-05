ef9255662e7aae249cdae40153d66aa36d1624e6
Anthony Mercado <antho@antho.local>
2024-12-05 17:53:52 -0600

    add meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Navigation from "@/components/Navigation";
 import LayloProvider from "@/context/LayloProvider";
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import VideoProvider from "@/context/VideoProvider";
 import SmoothScroll from "@/components/SmoothScroll";
@@ -30,6 +31,22 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/manifest.webmanifest" />
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window,document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+               fbq('init', '574999218820481'); 
+              fbq('track', 'PageView');           
+          `}
+        </Script>
       </head>
       <body className={`${inter.className} ${space.className}`}>
         <SmoothScroll>
@@ -39,6 +56,14 @@ export default function RootLayout({
                 <Navigation />
               </div>
               {children}
+              <noscript>
+                <img
+                  height="1"
+                  width="1"
+                  src="https://www.facebook.com/tr?id=574999218820481&ev=PageView&noscript=1"
+                  alt="meta-pixel"
+                />
+              </noscript>
             </LayloProvider>
           </VideoProvider>
         </SmoothScroll>
